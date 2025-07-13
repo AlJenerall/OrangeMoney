a9346cc41d73754159a482a675404320a2193699
AlJenerall <mdalimoubah98@gmail.com>
2025-07-13 19:21:14 +0300

    Delete OrangeMoneyy directory

--- OrangeMoneyy/config/orange_money.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'client_id' => 'mcpzniKFcMIDRUJIl3vpvEP6s5zABWQV',
-    'client_secret' => 'hsjP66JGXxPHLBFK5d9lLoOjpxw8qpjfyGrQZL6BPheJ',
-    'merchant_key' => '268671bb',
-    'base_url' => 'https://api.orange.com',
-];
--- OrangeMoneyy/config/orange_money1.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'client_id' => 'YOUR_CLIENT_ID',
-    'client_secret' => 'YOUR_CLIENT_SECRET',
-    'merchant_key' => 'YOUR_MERCHANT_KEY',
-    'base_url' => 'https://api.orange.com',
-];
